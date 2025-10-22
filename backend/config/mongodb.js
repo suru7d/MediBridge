01f731ada2de85b7535216e94f05b4e77697d2d6
Suraj Datir <sddatir123@gmail.com>
2025-10-23 00:56:59 +0530

    Initial commit: MediBridge project

---
+++ backend/config/mongodb.js
@@ -0,0 +1,10 @@
+import mongoose, { mongo } from "mongoose"
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected',()=> console.log("Database Connected"))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+}
+
+export default connectDB
